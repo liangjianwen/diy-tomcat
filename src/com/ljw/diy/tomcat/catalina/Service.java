37c83133dcdf8f298fe8109d737cdf45d9e1e92d
Jianwen Liang <liang.jianwen@outlook.com>
2021-10-15 19:30:22 +0800

    添加Server

--- src/com/ljw/diy/tomcat/catalina/Service.java
+++ src/com/ljw/diy/tomcat/catalina/Service.java
@@ -5,13 +5,19 @@ import com.ljw.diy.tomcat.util.ServerXMLUtil;
 public class Service {
     private String name;
     private Engine engine;
+    private Server server;
 
-    public Service(){
+    public Service(Server server){
         this.name = ServerXMLUtil.getServiceName();
         this.engine = new Engine(this);
+        this.server = server;
     }
 
     public Engine getEngine(){
         return engine;
+    }
+
+    public Server getServer(){
+        return server;
     }
 }
